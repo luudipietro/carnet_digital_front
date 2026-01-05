910b0462eb2dd6211436a936eecfc2bc462f3d6c
PC Luciano Di Pietro <huesodipietro@gmail.com>
2026-01-05 20:26:10 -0300

    configuracion para conectarse al back desplegado

--- src/app/socios/serviceDeudaSocio.ts
+++ src/app/socios/serviceDeudaSocio.ts
@@ -2,13 +2,14 @@ import { inject, Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http'
 import { Observable } from 'rxjs';
 import { DeudaSocio } from '../deudaSocio';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ServiceDeudaSocio {
-  private http = inject(HttpClient)
-  private baseUrl = 'http://192.168.100.82:5000/api/socios'
+  private http = inject(HttpClient);
+  private baseUrl = environment.apiUrl;
 
   obtenerDeudaSocio(cuit:string): Observable<DeudaSocio>{
     return this.http.get<DeudaSocio>(`${this.baseUrl}/${cuit}`)
